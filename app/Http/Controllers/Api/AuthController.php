2142f9615640e72c90c4944a03bdfb5274f5f9ad
ikara-py <ali16kara@gmail.com>
2026-03-10 11:32:16 +0000

    feat: Add login functionality to AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -26,5 +27,33 @@ class AuthController extends Controller
             ],
 
         ], 201);
+    }
+
+    public function login(Request $request): JsonResponse{
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'message' => 'invalid credentials',
+            ], 401);
+        }
+
+        $user->tokens()->delete();
+        $token = $user->createToken('auth-token');
+
+        return response()->json([
+            'message' => 'login successful',
+            'token' => $token,
+            'user' => [
+                'id' =>$user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ],
+        ], 200);
     }
 }
