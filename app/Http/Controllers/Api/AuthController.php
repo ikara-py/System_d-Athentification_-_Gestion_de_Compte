679703c1cb661e31fb44135ff8dab92ae132ef9b
ikara-py <ali16kara@gmail.com>
2026-03-10 12:08:24 +0000

    feat: Add logout functionality to AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -56,4 +56,11 @@ class AuthController extends Controller
             ],
         ], 200);
     }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'logout successful',
+        ],200);
+    }
 }
